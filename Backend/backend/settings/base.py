6b8ab8080cb4d4490fd679909a55c9cf8fb624bb
marwan01110 <marwanmohamed@feng.bu.edu.eg>
2026-01-08 23:26:29 +0200

    MYSQL Update

--- Backend/backend/settings/base.py
+++ Backend/backend/settings/base.py
@@ -64,11 +64,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 ASGI_APPLICATION = 'backend.asgi.application'
 
-# Database configuration: using SQLite for simplicity
+# Database configuration: using mysql for simplicity
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
